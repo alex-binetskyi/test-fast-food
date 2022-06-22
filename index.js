bc9a553060a37c41d82af457c20f4d70d60001ed
Alex Binetskyi <ab@softevol.com>
2022-06-22 17:28:50 +0300

    Test.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const web_link = "https://rozetka.com.ua/";
+const web_link = "https://slotokingua.com/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome to the Test Fast Food.", {
