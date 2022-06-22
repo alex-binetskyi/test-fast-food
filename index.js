24655d51f86df6304a5a8dbd9ba2eb3a2cdc543c
Alex Binetskyi <ab@softevol.com>
2022-06-22 16:56:45 +0300

    Test.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const web_link = "https://dancing-kulfi-85047d.netlify.app/";
+const web_link = "https://google.com";
 
 bot.start((ctx) =>
   ctx.reply("Welcome to the Test Fast Food.", {
