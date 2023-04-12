c377b93c5b222fdb2bc6ceaab6d6b57d0c99d950
alex-softevol <ab@softevol.com>
2023-04-12 22:30:09 +0300

    Change text

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const web_link = "https://dancing-kulfi-85047d.netlify.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome to the Fast Food.", {
+  ctx.reply("Welcome to the Test Form.", {
     reply_markup: {
-      keyboard: [[{ text: "Fast Food", web_app: { url: web_link } }]],
+      keyboard: [[{ text: "TEST FORM", web_app: { url: web_link } }]],
     },
   })
 );
